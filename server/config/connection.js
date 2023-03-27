a64b69be2469d669428cb3e3b09856c41a2dc715
Big Bob <devon.eadie@yahoo.com>
2023-03-27 15:35:02 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/BookSearchDb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/BookSearchDb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
